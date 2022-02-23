c4c38319a92285cbd4252946e1b08a59313bfbbc
Javier Otero <javier.otero@pagofx.com>
2022-02-23 15:33:10 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './demos/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Demo0Component } from './demos/demo0/demo0.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Demo0Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
